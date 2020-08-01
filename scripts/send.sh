727b19099bd2b8f97d6c2819d0ac57bb63724a25
generator-crypto <69088262+generator-crypto@users.noreply.github.com>
2020-08-02 06:47:08 +0700

    Update send.sh

--- scripts/send.sh
+++ scripts/send.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-generatorcli tx send  --gas auto --gas-adjustment 1.5 coingenerator $1 200000000ouro
+generatorcli tx send  --gas auto --gas-adjustment 1.5 coingenerator $1 200000000gnrt
