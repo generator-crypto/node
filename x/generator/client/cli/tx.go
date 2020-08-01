612cfe0b421f49cf7786bdb3908215613f8d3ddb
generator-crypto <69088262+generator-crypto@users.noreply.github.com>
2020-08-02 03:41:18 +0700

    Create tx.go

---
+++ x/generator/client/cli/tx.go
@@ -0,0 +1,81 @@
+package cli
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/cosmos/cosmos-sdk/client/context"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
+
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/generator-crypto/node/x/generator/types"
+)
+
+// GetTxCmd returns the transaction commands for this module
+func GetTxCmd(cdc *codec.Codec) *cobra.Command {
+	generatorTxCmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("%s transactions subcommands", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	generatorTxCmd.AddCommand(flags.PostCommands(
+		GetCmdChangeParams(cdc),
+		GetCmdRestoreUnbonding(cdc),
+	)...)
+
+	return generatorTxCmd
+}
+
+func GetCmdChangeParams(cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "change-params",
+		Short: "Changes the required param of the system",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+			inBuf := bufio.NewReader(cmd.InOrStdin())
+			txBldr := auth.NewTxBuilderFromCLI(inBuf).WithTxEncoder(utils.GetTxEncoder(cdc))
+
+			msg := types.NewMsgChangeParams(cliCtx.GetFromAddress())
+
+			err := msg.ValidateBasic()
+
+			if err != nil {
+				return err
+			}
+
+			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
+		},
+	}
+}
+
+func GetCmdRestoreUnbonding(cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "restore-unbonding",
+		Short: "Restores the unbonding coins",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+			inBuf := bufio.NewReader(cmd.InOrStdin())
+			txBldr := auth.NewTxBuilderFromCLI(inBuf).WithTxEncoder(utils.GetTxEncoder(cdc))
+
+			msg := types.NewMsgRestoreUnbonding(cliCtx.GetFromAddress())
+
+			err := msg.ValidateBasic()
+
+			if err != nil {
+				return err
+			}
+
+			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
+		},
+	}
+}
