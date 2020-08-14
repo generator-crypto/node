4a1f91670e9cf0dfd8b2e2efd75bae8ae1771922
generator-crypto <69088262+generator-crypto@users.noreply.github.com>
2020-08-14 22:02:57 +0700

    Update genesis.go

--- x/posmining/types/genesis.go
+++ x/posmining/types/genesis.go
@@ -1,30 +1,20 @@
 package types
 
-import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"time"
-)
-
 // GenesisState - all posmining state that must be provided at genesis
 type GenesisState struct {
-	Correction Correction
 	Records  []Posmining
 }
 
 // NewGenesisState creates a new GenesisState object
-func NewGenesisState(correction Correction, records []Posmining) GenesisState {
+func NewGenesisState(records []Posmining) GenesisState {
 	return GenesisState{
-		Correction: correction,
 		Records:  records,
 	}
 }
 
 // DefaultGenesisState - default GenesisState used by Cosmos Hub
 func DefaultGenesisState() GenesisState {
-	startDate := time.Date(2019, 9, 1, 0, 0, 0, 0, time.UTC)
-
 	return GenesisState{
-		Correction: Correction{StartDate: startDate, OpeningPrice: sdk.NewInt(100)},
 		Records:  make([]Posmining, 0),
 	}
 }
